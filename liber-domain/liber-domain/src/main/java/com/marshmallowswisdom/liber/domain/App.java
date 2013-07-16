6b3f2b8f73d6b1d6054bed22dcac513a8d9e1e9a
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2013-07-16 18:44:00 -0500

    new domain project

---
+++ liber-domain/liber-domain/src/main/java/com/marshmallowswisdom/liber/domain/App.java
@@ -0,0 +1,13 @@
+package com.marshmallowswisdom.liber.domain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
