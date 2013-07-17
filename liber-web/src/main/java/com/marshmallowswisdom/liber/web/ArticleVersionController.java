59e472dac95c10b086cf918d3c3d47faeac787a8
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2013-07-16 21:43:48 -0500

    removed unused imports

--- liber-web/src/main/java/com/marshmallowswisdom/liber/web/ArticleVersionController.java
+++ liber-web/src/main/java/com/marshmallowswisdom/liber/web/ArticleVersionController.java
@@ -1,10 +1,5 @@
 package com.marshmallowswisdom.liber.web;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
