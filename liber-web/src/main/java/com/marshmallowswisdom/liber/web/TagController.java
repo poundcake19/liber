69d778461413e811e754924d3c095cd0edbd3e12
Shawn Sparks <shawn.sparks@marshmallowswisdom.com>
2013-07-24 20:39:51 -0500

    removed obsoleted code now that tags are created via AJAX rather than a separate page

--- liber-web/src/main/java/com/marshmallowswisdom/liber/web/TagController.java
+++ liber-web/src/main/java/com/marshmallowswisdom/liber/web/TagController.java
@@ -1,31 +1,15 @@
 package com.marshmallowswisdom.liber.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.marshmallowswisdom.liber.domain.Tag;
 import com.marshmallowswisdom.liber.persistence.Repository;
-import com.marshmallowswisdom.liber.web.form.TagForm;
 
 @Controller
 public class TagController {
-
-	@RequestMapping(value="/createTag", method=RequestMethod.POST)
-	public ModelAndView saveTag( @ModelAttribute TagForm tag ) {
-		final Repository repository = new Repository();
-		final Integer parentId = tag.getParent();
-		final Tag domainTag = parentId == null ? new Tag( tag.getName() ) : 
-													new Tag( tag.getName(), 
-															repository.retrieveTag( parentId ) );
-		repository.saveTag( domainTag );
-		final ModelAndView mav = new ModelAndView( "saveTag" );
-		mav.addObject( "tag", tag );
-		return mav;
-	}
 	
 	@RequestMapping("/tags/{id}")
 	public ModelAndView viewTag( @PathVariable final int id ) {
--- liber-web/src/main/webapp/WEB-INF/freemarker/saveTag.ftl
+++
@@ -1,4 +0,0 @@
-<#assign title="Tag Saved!"/>
-<#include "/includes/header.ftl"/>
-	<p>${tag.name} tag successfully saved!</p>
-<#include "/includes/footer.ftl"/>
